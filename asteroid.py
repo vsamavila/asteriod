66cb6ae19e9b6ab961ecc814ce9099bd7e5ae850
vsamavila <vsamavilac@gmail.com>
2025-12-25 19:26:19 -0500

    game v1.0

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import *
 from constants import *
+from logger import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -13,3 +15,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self, ):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            print("this was a small asteroid and we're done")
+            return 
+
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        new_movement = self.velocity.rotate(angle)
+        new_movement2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first = Asteroid(self.position.x, self.position.y, new_radius)
+        second = Asteroid(self.position.x, self.position.y, new_radius)
+        first.velocity = new_movement * 1.2
+        second.velocity = new_movement2 * 1.2
+            
+        
--- main.py
+++ main.py
@@ -42,6 +42,12 @@ def main():
         updatable.update(dt)
 
         for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
             if player.collides_with(asteroid):
                 log_event("player_hit")
                 print("Game over!")
